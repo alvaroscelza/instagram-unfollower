0f92e98c7488063e46308b1f6755c18e189f5948
Alvaro Scelza <alvaroscelza@gmail.com>
2024-05-14 03:13:53 -0300

    Added dotenv to manage user and pass

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
-from selenium import webdriver
-from selenium.webdriver.edge.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-# Retrieve Instagram credentials from environment variables
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+
+load_dotenv()
 username = os.getenv('INSTAGRAM_USERNAME')
 password = os.getenv('INSTAGRAM_PASSWORD')
 
@@ -22,8 +24,8 @@ browser.get('https://www.instagram.com/')
 
 # Wait for the login fields to be present
 wait = WebDriverWait(browser, 10)
-username_field = wait.until(EC.presence_of_element_located((By.NAME, 'username')))
-password_field = wait.until(EC.presence_of_element_located((By.NAME, 'password')))
+username_field = wait.until(expected_conditions.presence_of_element_located((By.NAME, 'username')))
+password_field = wait.until(expected_conditions.presence_of_element_located((By.NAME, 'password')))
 
 # Enter your login credentials
 username_field.send_keys(username)
@@ -32,8 +34,3 @@ password_field.send_keys(password)
 # Find and click the login button
 login_button = browser.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]')
 login_button.click()
-
-# Optionally, wait for the home page to load
-wait.until(EC.presence_of_element_located((By.XPATH, '//div[text()="Home"]')))
-
-# You can now perform other automated tasks on Instagram
